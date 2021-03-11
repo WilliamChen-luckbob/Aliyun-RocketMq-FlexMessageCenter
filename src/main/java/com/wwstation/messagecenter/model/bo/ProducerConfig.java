754f9bc50475f5b5054d05f7bbd709bc1692b327
william@work <416934181@qq.com>
2021-03-11 10:14:39 +0800

    [fix]修复producer config未序列化异常

--- src/main/java/com/wwstation/messagecenter/model/bo/ProducerConfig.java
+++ src/main/java/com/wwstation/messagecenter/model/bo/ProducerConfig.java
@@ -2,6 +2,8 @@ package com.wwstation.messagecenter.model.bo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 生产者配置
  * @author william
@@ -9,7 +11,8 @@ import lombok.Data;
  * @Date: 2021-03-05 15:16
  */
 @Data
-public class ProducerConfig {
+public class ProducerConfig implements Serializable {
+    private static final long serialVersionUID = -6594234250498537679L;
     private String topic;
     private String instanceId;
 }
